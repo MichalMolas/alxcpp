34b414343e75141b7b621ec2d0773ead57575044
Jan Malinowski <jan.malinowski@gamil.com>
2018-03-11 14:38:25 +0000

    Fibo main.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+
 int main()
 {
-	printf("hello, world!\n");
+	std::cout << ("hello, world!\n");
+
+	int l1 = 0;
+	int l2 = 1;
+	int l;
+	int n;
+	std::cin >> n;
+
+	for (int i = 0; i < n-1; i++){
+			l = l1 + l2;
+			l1 = l2;
+			l2 = l;
+	}
+
+	std::cout << n << " liczba ciagu Fibo = " << l << '\n';
+
 	return 0;
 }
 
